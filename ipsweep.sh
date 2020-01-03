578f7892e677e768fa7aa8d8d8d296cfafdc966d
Bad3r <bad3r@protonmail.com>
2020-01-03 14:48:46 -0500

    added usage to pingsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -3,6 +3,11 @@
 # the scrip will ping the IPs range 192.168.1.1-192.168.1.254
 # and returns live hosts
 
-for ip in $(seq 1 254); do
-ping -c 1 "$1.$ip" | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-done
+if [ "$1" == "" ]
+then 
+    echo "Usage: ./ipsweep.sh 192.168.1"
+else
+    for ip in $(seq 1 254); do
+    ping -c 1 "$1.$ip" | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
