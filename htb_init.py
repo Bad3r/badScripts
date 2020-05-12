b9b0bd695933f53a2ded0428e65c2caa05c8bb94
Bad3r <bad3r@protonmail.com>
2020-05-12 15:29:34 -0400

    added htb_init.py desc. inside the file

--- htb_init.py
+++ htb_init.py
@@ -4,6 +4,15 @@ import socket
 import sys
 from shlex import quote
 
+# simple script given a box name and IP address
+# the script will create a folder for the box  and a sub folder called recon
+# it will also add an entery into /etc/hosts
+# ex: 10.10.10.188 cache.htb
+# to use the script change the path your HTB home directory
+# by modifiying the variable "directory"
+# USAGE: ./htb_init.py
+# NOTE: the script requires admin priviliege to append to etc/hosts
+
 def main():
     box = input("[!] Enter Box name: ").strip()
     boxIP = input("[!] Enter Box IP: ").strip()
@@ -27,7 +36,7 @@ def main():
 
     print(f"[*] Adding host: {hostname} IP: {boxIP} to /etc/hosts..")
     command = f"echo '{boxIP} {box.lower()}.htb' >> /etc/hosts"
-    os.system(command)
+    os.system(quote(command))
     print("[*] Done!")
 
 if __name__ == "__main__":
