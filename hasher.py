3fee248e85544720df81cb8264af954495c514e2
Bad3r <bad3r@protonmail.com>
2021-03-05 09:09:03 -0500

    added hasher.py unix only

---
+++ hasher.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import sys
+import os
+import hashlib
+
+# prefix components:
+space =  '    '
+branch = '│   '
+# pointers:
+tee =    '├── '
+last =   '└── '
+
+  
+    
+    
+
+def tree(dir_path: Path, prefix: str=''):
+    """A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+    """
+    BUF_SIZE = 65536  # read stuff in 64kb chunks
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        
+        if path.is_dir(): # extend the prefix and recurse:
+            extension = branch if pointer == tee else space 
+            # i.e. space because last, └── , above so no more |
+            yield prefix + pointer + path.name
+            yield from tree(path, prefix=prefix+extension)
+        else:
+            md5 = hashlib.md5()
+            sha1 = hashlib.sha1()
+            try:
+                with open(f"{path}", 'rb') as f:
+                    while True:
+                        data = f.read(BUF_SIZE)
+                        if not data:
+                            break
+                        md5.update(data)
+                        sha1.update(data)
+                        
+                        yield f"{prefix}{pointer}{path.name}, MD5={md5.hexdigest()}, SHA1={sha1.hexdigest()}"
+            except FileNotFoundError:
+                pass
+
+def main():
+
+    for line in tree(Path("/bin")):
+        print(line)
+    
+
+main()
